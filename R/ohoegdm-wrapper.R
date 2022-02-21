c61dc573e6a24b45c0d9c522411df0653baa112c
James Balamuta <balamut2@illinois.edu>
2022-02-21 11:09:19 -0600

    Documentation updates round 1

--- R/ohoegdm-wrapper.R
+++ R/ohoegdm-wrapper.R
@@ -1,7 +1,8 @@
 # Construct the EXMS object
 new_ohoegdm_model = function(model_mcmc,
                              details,
-                             estimates = NULL) {
+                             estimates = NULL,
+                             recovery = NULL) {
 
     estimate_chain = lapply(model_mcmc, summarize_model)
     estimates = c(estimate_chain, estimates)
@@ -10,7 +11,8 @@ new_ohoegdm_model = function(model_mcmc,
         list(
             estimates = estimates, # Iterates over each parameter and obtains summary information
             chain = chain,
-            details = details
+            details = details,
+            recovery = recovery
         ),
         class = c("ohoegdm", "egdm")
     )
@@ -22,51 +24,77 @@ new_ohoegdm_model = function(model_mcmc,
 #'
 #' Performs the Gibbs sampling routine for a higher-order EGDM.
 #'
-#' @param y                 Item Matrix
+#' @param y                 Ordinal Item Matrix
 #' @param k                 Dimension to estimate for Q matrix
 #' @param m                 Number of Item Categories. Default is `2` matching the binary case.
 #' @param order             Highest interaction order to consider. Default model-specified `k`.
-#' @param sdMH              Metropolis-Hastings standard deviation tuning parameter. 
+#' @param sd_mh             Metropolis-Hastings standard deviation tuning parameter. 
 #' @param burnin            Amount of Draws to Burn
 #' @param chain_length      Number of Iterations for chain.
-#' @param l0,l1,m0,bq Additional tuning parameters.
+#' @param l0                Spike parameter. Default 1 for intercept and 100
+#'                          coefficients
+#' @param l1                Slab parameter. Default 1 for all values. 
+#' @param m0,bq             Additional tuning parameters.
 #' @return
 #'
-#' A `egdm_delta` object containing three named lists:
+#' A `ohoegdm` object containing four named lists:
 #'
-#' **`estimates`**
+#' **`estimates`**: Averaged chain iterations
 #'
 #' - `thetas`: Average theta coefficients
 #' - `betas`: Average beta coefficients
-#' - `class`: Average class membership
-#' - `pi`: Average attribute class probability.
-#' - `omega`: Average omega
+#' - `deltas`: Average activeness of coefficients
+#' - `classes`: Average class membership
+#' - `m2lls`: Average negative two times log-likelihood
+#' - `omegas`: Average omega
+#' - `kappas` : Average category threshold parameter
+#' - `taus`: Average $K$-vectors of factor intercept
+#' - `lambdas`: Average $K$-vectors of factor loadings
+#' - `guessing`: Average guessing item parameter
+#' - `slipping`: Average slipping item parameter
+#' - `QS`: Average activeness of Q matrix entries
+#' - `MHsum`: Average acceptance from metropolis hastings sampler
+#' 
+#' **`chain`**: Chain iterations from the underlying _C++_ rountine.
 #'
-#' **`chain`**
+#' - `thetas`: Theta coefficients iterations
+#' - `betas`:  Beta coefficients iterations
+#' - `deltas`: Activeness of coefficients iterations
+#' - `classes`:  Class membership iterations
+#' - `m2lls`: Negative two times log-likelihood iterations
+#' - `omegas`:  Omega iterations
+#' - `kappas` : Category threshold parameter iterations
+#' - `taus`: $K$-vectors of factor intercept iterations
+#' - `lambdas`: $K$-vectors of factor loadings iterations
+#' - `guessing`: Guessing item parameter iterations
+#' - `slipping`: Slipping item parameter iterations
 #'
-#' - `thetas`: theta coefficients iterations
-#' - `betas`:  beta coefficients iterations
-#' - `class`:  class membership iterations
-#' - `omega`:  omega iterations
-#'
-#' **`details`**
+#' **`details`**: Properties used to estimate the model
 #'
 #' - `n`: Number of Subjects
 #' - `j`: Number of Items
 #' - `k`: Number of Traits
+#' - `m`: Number of Item Categories.
 #' - `order`: Highest interaction order to consider. Default model-specified `k`.
-#' - `sdMH`: Metropolis-Hastings standard deviation tuning parameter. 
+#' - `sd_mh`: Metropolis-Hastings standard deviation tuning parameter. 
 #' - `l0`: Spike parameter
 #' - `l1`: Slab parameter
 #' - `m0`, `bq`: Additional tuning parameters
 #' - `burnin`: Number of Iterations to discard
 #' - `chain_length`: Number of Iterations to keep
+#' - `runtime`: Elapsed time algorithm run time in the _C++_ code.
 #'
+#' **`recovery`**: Assess recovery metrics under a simulation study.
+#' 
+#' - `Q_item_encoded`: Per-iteration item encodings from Q matrix.
+#' 
 #' @details
 #'
 #' The **`estimates`** list contains the mean information from the sampling
-#' procedure. Meanwhile, the **`chain`** list contains full MCMC values. Lastly,
+#' procedure. Meanwhile, the **`chain`** list contains full MCMC values. Moreover,
 #' the **`details`** list provides information regarding the estimation call.
+#' Lastly, the **`recovery`** list stores values that can be used when
+#' assessing the method under a simulation study.
 #'
 #' @rdname ohoegdm
 #' @export
@@ -150,7 +178,7 @@ new_ohoegdm_model = function(model_mcmc,
 #' # If sample size is:
 #' #  - small, allow for larger standard deviation
 #' #  - large, allow for smaller standard deviation.
-#' sdMH = .4 
+#' sd_mh = .4 
 #' burnin = 50        # Set for demonstration purposes, increase to at least 5,000 in practice.
 #' chain_length = 100 # Set for demonstration purposes, increase to at least 40,000 in practice.
 #' 
@@ -167,7 +195,7 @@ new_ohoegdm_model = function(model_mcmc,
 #'   l1 = l1s,
 #'   m0 = 0,
 #'   bq = 1,
-#'   sdMH = sdMH,
+#'   sd_mh = sd_mh,
 #'   burnin = burnin,
 #'   chain_length = chain_length
 #' )
@@ -176,7 +204,7 @@ ohoegdm = function(y,
                  k,
                  m = 2,
                  order = k,
-                 sdMH = 0.4,
+                 sd_mh = 0.4,
                  burnin = 1000L,
                  chain_length = 10000L,
                  l0 = c(1, rep(100, sum(choose(k, seq_len(order))))),
@@ -199,7 +227,7 @@ ohoegdm = function(y,
             Y = y, K = k, M = m, order = order,
             l0 = l0, l1 = l1,
             m0 = m0, bq = bq,
-            sdMH = sdMH,
+            sdMH = sd_mh,
             burnin = burnin, chain_length = chain_length)
     })
 
@@ -214,7 +242,7 @@ ohoegdm = function(y,
             k = k,
             m = m,
             order = order,
-            sdMH = sdMH,
+            sd_mh = sd_mh,
             l0 = l0,
             l1 = l1,
             m0 = m0,
@@ -222,7 +250,8 @@ ohoegdm = function(y,
             burnin = burnin,
             chain_length = chain_length,
             runtime = timing[["elapsed"]]
-        )
+        ),
+        recovery = model_mcmc$recovery
     )
 }
 
